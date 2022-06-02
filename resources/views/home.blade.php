f257c9c81726818b83d6828c19fe802a8dc62397
Tahir Ali <tahir.56ali@gmail.com>
2022-06-02 18:27:41 +0500

    tweets timeline and creating new tweet functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,14 @@
             @include('_sidebar-links')
         </div>
 
-        <div class="lg:flex-1">
-            2
+        <div class="lg:flex-1 lg:mx-10 lg:mb-10" style="max-width: 700px;">
+            @include('_publish-tweet-panel')
+
+            <div class="border border-gray-300 rounded-lg">
+                @foreach($tweets as $tweet)
+                    @include('_tweet')
+                @endforeach
+            </div>
         </div>
 
         <div class="lg:w-1/4">
