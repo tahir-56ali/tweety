6f9cb73f7e1a8cc8848ce97124f4f52dfcff8106
Tahir Ali <tahir.56ali@gmail.com>
2022-06-02 16:16:14 +0500

    design the timeline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="lg:flex">
+        <div class="lg:w-1/4">
+            @include('_sidebar-links')
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="lg:flex-1">
+            2
+        </div>
 
-                    You are logged in!
-                </div>
-            </div>
+        <div class="lg:w-1/4">
+            @include('_friends-list')
         </div>
     </div>
-</div>
 @endsection
