12a83f3908d57f77a33e53ab53b3b6a0bcbc71fe
Tahir Ali <tahir.56ali@gmail.com>
2022-06-03 20:49:29 +0500

    expanding the timeline

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,16 +1,19 @@
-<h3 class="font-bold text-xl mb-4">Following</h3>
-<ul>
-    @foreach(auth()->user()->follows as $user)
-        <li class="mb-4">
-            <div class="flex items-center text-sm">
-                <img
-                    src="{{ $user->avatar }}"
-                    alt=""
-                    class="rounded-full mr-2"
-                >
+<div class="bg-gray-200 border border-gray-300 rounded-lg py-4 px-6">
+    <h3 class="font-bold text-xl mb-4">Following</h3>
 
-                {{ $user->name }}
-            </div>
-        </li>
-    @endforeach
-</ul>
+    <ul>
+        @foreach(auth()->user()->follows as $user)
+            <li class="mb-4">
+                <div class="flex items-center text-sm">
+                    <img
+                        src="{{ $user->avatar }}"
+                        alt=""
+                        class="rounded-full mr-2"
+                    >
+
+                    {{ $user->name }}
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</div>
